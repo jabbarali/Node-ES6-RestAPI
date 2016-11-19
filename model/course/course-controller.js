14b2611cedbfb6c2a7976dc7913ffacd90e8434e
unknown <jabbar83@gmail.com>
2016-11-19 23:45:10 +0000

    removing moved files

--- model/course/course-controller.js
+++
@@ -1,6 +0,0 @@
-const Controller = require('../../lib/controller');
-const courseFacade  = require('./course-facade');
-
-class CourseController extends Controller {}
-
-module.exports = new CourseController(courseFacade);
